5964d36f5be3eccf9b689c88c23f0c796fa3226e
Victor Parizio <victorparizio@hotmail.com>
2023-03-29 10:11:06 -0300

    feat (App) useRef Nesta aula vimos como armazenar valores anteriores de um input usando useRef. Estes valores podem ser usado posteriormente para comparações, etc.

--- react-ref/src/App.js
+++ react-ref/src/App.js
@@ -3,6 +3,63 @@ import React, { useState, useEffect, useRef } from "react";
 export default function App() {
 
     const [texto, setTexto] = useState('')
+    const anterior = useRef('')
+
+    useEffect(() => {
+        anterior.current = texto
+    }, [texto])
+
+    function executar(){}
+
+    return (
+        <>
+            <h1>React Hooks - useRef</h1>
+            <hr />
+            <input type="text" onChange={e => { setTexto(e.target.value) }} />
+            <br />
+            <hr />
+            <button onClick={executar}>Executar</button>
+            <p>Texto: <strong>{texto}</strong> (texto anterior) {anterior.current} </p>
+        </>
+    )
+}
+
+//----------------------------
+
+/*
+import React, { useState, useRef } from "react";
+
+export default function App() {
+
+    const [texto, setTexto] = useState('')
+    const inputRef = useRef()
+
+    function executar() {
+        console.log(inputRef.current.value)
+        setTexto(inputRef.current.value)
+    }
+
+    return (
+        <>
+            <h1>React Hooks - useRef</h1>
+            <hr />
+            <input ref={inputRef} type="text" />
+            <br />
+            <hr />
+            <button onClick={executar}>Executar</button>
+            <p>Texto: <strong>{texto}</strong></p>
+        </>
+    )
+}*/
+
+//----------------------------
+
+/*
+import React, { useState, useEffect, useRef } from "react";
+
+export default function App() {
+
+    const [texto, setTexto] = useState('')
     const total = useRef(1)
 
     useEffect(()=>{
@@ -11,7 +68,7 @@ export default function App() {
 
     return (
         <>
-            <h1>React Hooks - useEffect</h1>
+            <h1>React Hooks - useRef</h1>
             <hr />
             <input type="text" value={texto} onChange={event => setTexto(event.target.value)} />
             <hr />
@@ -20,4 +77,4 @@ export default function App() {
             <p>Total: {total.current}</p>
         </>
     )
-}
+}*/
