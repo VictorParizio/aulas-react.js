13330eb79fbdd00d591e09435361862ea2982174
Victor Parizio <victorparizio@hotmail.com>
2023-04-05 11:21:33 -0300

    feat (Home) useNavigate Vimos como usar o "useNavigate" dentro de componentes para navergar pelas rotas dentro da aplicação.

--- 15-react-route/src/Componentes/Home.jsx
+++ 15-react-route/src/Componentes/Home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom'
 
 export default function Home() {
+
+    const irPara = useNavigate()
+
     return (
         <section className="container">
             <div className="row justify-content-center mt-3">
                 <div className="col-8 card bg-light text-center p-3">
                     <h2>Home!</h2>
+                </div>
+                <div className="text-center mt-4">
+                    <button onClick={() => {
+                        irPara('/services')
+                    }}>Serviços</button>
                 </div>
             </div>
         </section>
