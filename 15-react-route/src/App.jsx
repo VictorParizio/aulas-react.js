7dd8e767539319d0bc9531a729d903f3adb7cba5
Victor Parizio <victorparizio@hotmail.com>
2023-04-05 09:52:52 -0300

    feat (App) Definindo Rotas Foi utilizado o Link para renderizar as rotas sem atualizar a página.

--- 15-react-route/src/App.jsx
+++ 15-react-route/src/App.jsx
@@ -1,19 +1,28 @@
-import { React } from 'react'
-import { Router, Route } from 'react-router-dom'
+import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
 
 import Home from './Componentes/Home'
 import Services from './Componentes/Services'
 import Contato from './Componentes/Contato'
+import Erro from './Componentes/Erro'
 
 export default function App() {
   return (
     <>
-      <h1>React Router Appe!</h1>
-      <Router>
-        <Route path='/' elemente={<Home />} />
-        <Route path='/services' elemente={<Services />} />
-        <Route path='/contato' elemente={<Contato />} />
-      </Router>
+      <h1>React Router App!</h1>
+
+      <nav>
+        <Link to='/'>Home</Link> | 
+        <Link to='/services'>Services</Link> | 
+        <Link to='/contato'>Contato</Link>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/contato' element={<Contato />} />
+        <Route path='/*' element={<Erro />} />
+      </Routes>
     </>
   )
 }
